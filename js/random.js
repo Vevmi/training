8c424c33e7c6718154a782998fca7ac28d4f9866
Vevmi <timofey.yastremskiy@gmail.com>
2024-02-06 17:20:14 +0300

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,7 +1,8 @@
 const random = Math.round(Math.random() * 800);
 let userNum = null;
+ prompt('Угадай число от 0 до 800. Для начала игры введите ваше число:');
 while (userNum !== random) {
-  userNum = +prompt('Enter your number:');
+  userNum = +prompt('Попробуй еще раз:');
   if (userNum > random) {
     alert('Бери меньше');
   } else {
